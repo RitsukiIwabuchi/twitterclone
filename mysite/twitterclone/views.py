e1c0888b2cfce9d53a6f0ee59700363950024309
github user name <r_iwabuchi@jwts.co.jp>
2021-09-14 06:52:07 +0900

    create tweet function

--- mysite/twitterclone/views.py
+++ mysite/twitterclone/views.py
@@ -3,8 +3,10 @@ from django.urls import reverse, reverse_lazy
 from django.views import generic
 from django.utils import timezone
 from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
-from .forms import SignUpForm
+from .forms import SignUpForm, TweetsForm
+from .models import Tweets
 
 class IndexView(generic.TemplateView):
     template_name = 'twitterclone/index.html'
@@ -22,3 +24,37 @@ class SignUpView(generic.CreateView):
         login(self.request, user)
         self.object = user
         return HttpResponseRedirect(self.get_success_url())
+
+class CreateTweet(LoginRequiredMixin, generic.CreateView):
+    form_class = TweetsForm
+    template_name = "twitterclone/tweets_create.html"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+class MyTweets(generic.ListView):
+    model = Tweets
+    template_name = "twitterclone/tweets_list.html"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.select_related('user').filter(user__username__iexact=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tweet_user"] = self.request.user
+        return context
+
+class TweetDelete(LoginRequiredMixin, generic.DeleteView):
+    model = Tweets
+    success_url = reverse_lazy("twitterclone:delete_done")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user_id=self.request.user.id)
+
+class DeleteDone(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'twitterclone/tweets_delete_done.html'
