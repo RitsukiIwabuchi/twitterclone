4aaa544f790d43852df5951b662fbbdbac2dbd44
github user name <r_iwabuchi@jwts.co.jp>
2021-09-22 22:35:13 +0900

    delete print_method

--- mysite/twitterclone/views.py
+++ mysite/twitterclone/views.py
@@ -46,7 +46,6 @@ class MyTweetsView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["tweet_user"] = self.request.user
-        print(context)
         return context
 
 class TweetDeleteView(LoginRequiredMixin, generic.DeleteView):
